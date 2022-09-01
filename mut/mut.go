e50a5f1049d81ce36a96c3369ac929bd87b57d65
Eduard Diachenko <handsomefox.hf@gmail.com>
2022-09-01 17:42:57 +0300

    mut: rename recievers and internal data

--- mut/mut.go
+++ mut/mut.go
@@ -8,67 +8,67 @@ import "sync"
 // This should better indicate which data is mutex protected and make
 // coding less error prone.
 type Mutex[T any] struct {
-	mdata *MutexData[T]
+	mutexData *MutexData[T]
 }
 
 // MutexData is the actual data.
 // It is a separate struct in order for us to avoid copying mutexes.
 type MutexData[T any] struct {
-	val T
-	mu  sync.Mutex
+	value T
+	mutex sync.Mutex
 }
 
 // New returns a new mutex ptr.
 func New[T any](data T) *Mutex[T] {
 	return &Mutex[T]{
-		mdata: &MutexData[T]{
-			mu:  sync.Mutex{},
-			val: data,
+		mutexData: &MutexData[T]{
+			mutex: sync.Mutex{},
+			value: data,
 		},
 	}
 }
 
 // Lock is the same as sync.Mutex.Lock().
-func (m *Mutex[T]) Lock() {
-	m.mdata.mu.Lock()
+func (mutex *Mutex[T]) Lock() {
+	mutex.mutexData.mutex.Lock()
 }
 
 // Unlock is the same as sync.Mutex.Unlock().
-func (m *Mutex[T]) Unlock() {
-	m.mdata.mu.Unlock()
+func (mutex *Mutex[T]) Unlock() {
+	mutex.mutexData.mutex.Unlock()
 }
 
 // TryLock is the same as sync.Mutex.TryLock().
-func (m *Mutex[T]) TryLock() bool {
-	return m.mdata.mu.TryLock()
+func (mutex *Mutex[T]) TryLock() bool {
+	return mutex.mutexData.mutex.TryLock()
 }
 
 // Ptr returns a pointer to internal data without locking.
-func (m *Mutex[T]) Ptr() *T {
-	return &m.mdata.val
+func (mutex *Mutex[T]) Ptr() *T {
+	return &mutex.mutexData.value
 }
 
 // Data returns a copy of internal data with locking.
-func (m *Mutex[T]) DataLocked() T {
-	m.Lock()
-	data := m.mdata.val
-	m.Unlock()
+func (mutex *Mutex[T]) DataLocked() T {
+	mutex.Lock()
+	data := mutex.mutexData.value
+	mutex.Unlock()
 	return data
 }
 
 // Data returns a copy the internal data without locking.
-func (m *Mutex[T]) Data() T {
-	return m.mdata.val
+func (mutex *Mutex[T]) Data() T {
+	return mutex.mutexData.value
 }
 
 // SetLocked changes internal value with locking.
-func (m *Mutex[T]) SetLocked(other T) {
-	m.Lock()
-	m.mdata.val = other
-	m.Unlock()
+func (mutex *Mutex[T]) SetLocked(other T) {
+	mutex.Lock()
+	mutex.mutexData.value = other
+	mutex.Unlock()
 }
 
 // Set changes internal value without locking.
-func (m *Mutex[T]) Set(other T) {
-	m.mdata.val = other
+func (mutex *Mutex[T]) Set(other T) {
+	mutex.mutexData.value = other
 }
